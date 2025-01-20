93413967f828723596c5ef22065b56b0e4bb68e5
Lauren Lajas <29675370+llajas@users.noreply.github.com>
2025-01-20 06:20:50 -0600

    refactor: migrate libvirt provider configuration to virtual directory

--- metal/terraform/provider.tf
+++
@@ -1,9 +0,0 @@
-variable "libvirt_uri" {
-  description = "URI for the Libvirt provider"
-  type        = string
-  default     = "qemu+ssh://root@10.8.0.8/system?keyfile=/home/red/.ssh/id_rsa&no_verify=1"
-}
-
-provider "libvirt" {
-  uri = var.libvirt_uri
-}
