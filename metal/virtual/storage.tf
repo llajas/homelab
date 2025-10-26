50a284ba4976b9265bc76dfe36557b306f19e558
Lauren Lajas <29675370+llajas@users.noreply.github.com>
2025-10-26 18:31:27 -0500

    feat(vm): revert to default storage pool name

--- metal/virtual/storage.tf
+++ metal/virtual/storage.tf
@@ -1,4 +1,4 @@
-resource "libvirt_pool" "homelab_pool" {
+resource "libvirt_pool" "default_pool" {
   name = var.storage_pool_name
   type = "dir"
   target {
@@ -10,6 +10,8 @@ resource "libvirt_volume" "vm_disks" {
   for_each = var.virtual_nodes
 
   name = "${each.value.name}.qcow2"
-  pool = libvirt_pool.homelab_pool.name
+  pool = libvirt_pool.default_pool.name
   size = each.value.disk_size * 1024 * 1024 * 1024
+
+  depends_on = [libvirt_pool.default_pool]
 }
